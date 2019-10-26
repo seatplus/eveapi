039c2e3f502394a8aedc8c8d102043f609da7550
Herpaderp Aldent <felix.a.huber@gmx.net>
2019-10-26 16:28:28 +0200

    move redis host from config to config

--- src/Config/eveapi.config.php
+++ src/Config/eveapi.config.php
@@ -7,6 +7,5 @@ return [
     // API Joblog logging
     'enable_joblog' => false,
 
-    'redis_cache_location' => env('REDIS_HOST', '127.0.0.1'),
     'eseye_loglevel' => 'info', // valid entries are RFC 5424 levels ('debug', 'info', 'warn', 'error')
 ];
