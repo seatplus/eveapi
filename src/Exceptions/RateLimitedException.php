71c5745e02f098db74cd306baf46d93306d349c1
Herpaderp Aldent <felix.a.huber@gmx.net>
2019-11-06 20:32:14 +0100

    add message to exception

--- src/Exceptions/RateLimitedException.php
+++ src/Exceptions/RateLimitedException.php
@@ -9,6 +9,8 @@ class RateLimitedException extends Exception
     public function __construct($message = '', $code = 0, Throwable $previous = null)
     {
 
+        $message = 'Application has produced to many errors. Either the refresh_token has been removed or scope was not granted';
+
         parent::__construct($message, $code, $previous);
     }
 }
