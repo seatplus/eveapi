c970b9255c00ef643dd26fbe6d81779f03c700fc
Herpaderp Aldent <felix.a.huber@gmx.net>
2019-11-09 09:29:26 +0100

    refactor routes to use route prefix

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -6,7 +6,12 @@ Route::namespace('Seatplus\Eveapi\Http\Controllers\Updates')
     ->prefix('eveapi')
     ->group(function () {
 
-        Route::post('/character_info', 'CharacterInfoController@update')->name('update.character_info');
+        Route::prefix('character')
+            ->group(function () {
+
+                Route::post('info', 'CharacterInfoController@update')->name('update.character_info');
+            });
+
         Route::post('/alliance_info', 'AllianceInfoController@update')->name('update.alliance_info');
         Route::post('/corporation_info', 'CorporationInfoController@update')->name('update.corporation_info');
 
