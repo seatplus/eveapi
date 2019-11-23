27d9fee012b4ad4964755344845c598789090542
Herpaderp Aldent <felix.a.huber@gmx.net>
2019-11-23 21:20:41 +0100

    Add RedisFunnelMiddleware

---
+++ src/Jobs/Middleware/RedisFunnelMiddleware.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace Seatplus\Eveapi\src\Jobs\Middleware;
+
+
+use Illuminate\Support\Facades\Redis;
+
+class RedisFunnelMiddleware
+{
+    /**
+     * Process the queued job.
+     *
+     * @param  mixed  $job
+     * @param  callable  $next
+     * @return mixed
+     */
+    public function handle($job, $next)
+    {
+
+        $key = (string) get_class($job) . ':' . implode($job->tags());
+
+        Redis::funnel($key)->limit(1)->then(function () use ($job, $next){
+
+            return $next($job);
+
+        });
+
+        return $job->delete();
+
+    }
+
+}
