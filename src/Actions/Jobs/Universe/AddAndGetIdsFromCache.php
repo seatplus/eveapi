f66edc09b0c31066a472c5693c1dc2e8d3e3e380
Herpaderp Aldent <felix.a.huber@gmx.net>
2019-12-09 16:17:39 +0000

    Apply fixes from StyleCI

--- src/Actions/Jobs/Universe/AddAndGetIdsFromCache.php
+++ src/Actions/Jobs/Universe/AddAndGetIdsFromCache.php
@@ -1,8 +1,6 @@
 <?php
 
-
 namespace Seatplus\Eveapi\Actions\Jobs\Universe;
-
 
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Cache;
@@ -45,5 +43,4 @@ class AddAndGetIdsFromCache
 
         return $this->ids_to_return;
     }
-
 }
