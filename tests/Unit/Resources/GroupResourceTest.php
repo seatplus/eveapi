06831096219babd80df3bbbeee587b579213b9a9
Herpaderp Aldent <felix.a.huber@gmx.net>
2020-01-19 09:28:32 +0100

    added resource tests

---
+++ tests/Unit/Resources/GroupResourceTest.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace Seatplus\Eveapi\Tests\Unit\Resources;
+
+
+use DMS\PHPUnitExtensions\ArraySubset\Assert;
+use Seatplus\Eveapi\Http\Resources\Group as GroupResource;
+use Seatplus\Eveapi\Models\Universe\Group;
+use Seatplus\Eveapi\Tests\TestCase;
+
+class GroupResourceTest extends TestCase
+{
+    public function testCorrectDataIsReturnedInResponse()
+    {
+        $resource = (new GroupResource($group = factory(Group::class)->create()))->jsonSerialize();
+
+        Assert::assertArraySubset([
+            'name' => $group->name
+        ], $resource);
+
+    }
+
+}
