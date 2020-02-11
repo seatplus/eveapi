57aff812443703d2f1022bccd269fa21f36589ef
Herpaderp Aldent <felix.a.huber@gmx.net>
2020-02-11 10:09:20 +0000

    Apply fixes from StyleCI

--- src/Listeners/ReactOnFreshRefreshToken.php
+++ src/Listeners/ReactOnFreshRefreshToken.php
@@ -1,8 +1,30 @@
 <?php
 
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019, 2020 Felix Huber
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 namespace Seatplus\Eveapi\Listeners;
-
 
 use Seatplus\Eveapi\Containers\JobContainer;
 use Seatplus\Eveapi\Events\RefreshTokenSaved;
@@ -20,7 +42,7 @@ class ReactOnFreshRefreshToken
     {
 
         $job_container = new JobContainer([
-            'refresh_token' => $refresh_token_event->refresh_token->refresh()
+            'refresh_token' => $refresh_token_event->refresh_token->refresh(),
         ]);
 
         //TODO queue all character job and before corporation with chain, the members to check if user has access to certain corp informations.
@@ -35,5 +57,4 @@ class ReactOnFreshRefreshToken
 
         CharacterRoleJob::dispatch($job_container)->onQueue('high');
     }
-
 }
