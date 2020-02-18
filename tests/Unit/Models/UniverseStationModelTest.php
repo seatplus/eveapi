d3c0a744ea262b65edd2bddea300f54e401703f2
Herpaderp Aldent <felix.a.huber@gmx.net>
2020-02-18 08:58:30 +0100

    Refactor scopes

--- tests/Unit/Models/UniverseStationModelTest.php
+++ tests/Unit/Models/UniverseStationModelTest.php
@@ -4,12 +4,6 @@
 namespace Seatplus\Eveapi\Tests\Unit\Models;
 
 
-use Faker\Factory;
-use Seatplus\Eveapi\Models\Alliance\AllianceInfo;
-use Seatplus\Eveapi\Models\Character\CharacterAffiliation;
-use Seatplus\Eveapi\Models\Character\CharacterInfo;
-use Seatplus\Eveapi\Models\Corporation\CorporationInfo;
-use Seatplus\Eveapi\Models\RefreshToken;
 use Seatplus\Eveapi\Models\Universe\Station;
 use Seatplus\Eveapi\Models\Universe\System;
 use Seatplus\Eveapi\Tests\TestCase;
