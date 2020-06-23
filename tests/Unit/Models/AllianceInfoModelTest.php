d0877742b362ebdde093b0629b61434c33a9d021
Herpaderp Aldent <felix.a.huber@gmx.net>
2020-06-23 16:51:28 +0200

    prepare package for refactoring affiliations

--- tests/Unit/Models/AllianceInfoModelTest.php
+++ tests/Unit/Models/AllianceInfoModelTest.php
@@ -5,6 +5,9 @@ namespace Seatplus\Eveapi\Tests\Unit\Models;
 
 
 use Seatplus\Eveapi\Models\Alliance\AllianceInfo;
+use Seatplus\Eveapi\Models\Character\CharacterAffiliation;
+use Seatplus\Eveapi\Models\Character\CharacterInfo;
+use Seatplus\Eveapi\Models\Corporation\CorporationInfo;
 use Seatplus\Eveapi\Models\SsoScopes;
 use Seatplus\Eveapi\Tests\TestCase;
 
@@ -18,6 +21,29 @@ class AllianceInfoModelTest extends TestCase
         $alliance_info->ssoScopes()->save(factory(SsoScopes::class)->make());
 
         $this->assertInstanceOf(SsoScopes::class, $alliance_info->refresh()->ssoScopes);
+    }
+
+    /** @test */
+    public function it_has_character_affiliation()
+    {
+        $affiliation = $this->test_character->character_affiliation;
+
+        if(!$affiliation->alliance_id) {
+            $alliance = factory(AllianceInfo::class)->create();
+            $affiliation->alliance_id = $alliance->alliance_id;
+            $affiliation->save();
+        }
+
+        $this->assertNotNull($affiliation->alliance_id);
+
+        $alliance = $affiliation->alliance;
+
+        $this->assertInstanceOf(AllianceInfo::class, $alliance);
+
+        $this->assertInstanceOf(CharacterInfo::class, $alliance->characters->first());
+
+        $this->assertEquals($this->test_character->character_id, $alliance->characters->first()->character_id);
+
     }
 
 }
