b879a4d5e38fc087fdd28c18244b680fe308475d
Herpaderp Aldent <felix.a.huber@gmx.net>
2020-07-31 22:47:01 +0200

    Add CorporationMemberTrackingJob and Test

---
+++ tests/Jobs/Corporation/CorporationMemberTrackingJobTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Seatplus\Eveapi\Tests\Jobs\Corporation;
+
+use Illuminate\Support\Facades\Queue;
+use Seatplus\Eveapi\Containers\JobContainer;
+use Seatplus\Eveapi\Jobs\Character\CharacterRoleJob;
+use Seatplus\Eveapi\Jobs\Corporation\CorporationMemberTrackingJob;
+use Seatplus\Eveapi\Tests\TestCase;
+
+class CorporationMemberTrackingJobTest extends TestCase
+{
+    /**
+     * @test
+     * @runTestsInSeparateProcesses
+     */
+    public function testIfJobIsQueued()
+    {
+        Queue::fake();
+
+        // Assert that no jobs were pushed...
+        Queue::assertNothingPushed();
+
+       $job_container = new JobContainer([
+            'refresh_token' => $this->test_character->refresh_token
+        ]);
+
+        CorporationMemberTrackingJob::dispatch($job_container)->onQueue('default');
+
+        // Assert a job was pushed to a given queue...
+        Queue::assertPushedOn('default', CorporationMemberTrackingJob::class);
+    }
+
+}
