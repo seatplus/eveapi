a3a77cef03b205008060a6f0eda13004c3fbd6bd
Herpaderp Aldent <felix.a.huber@gmx.net>
2020-09-01 21:34:36 +0200

    Add global settings

---
+++ src/Helpers/helpers.php
@@ -0,0 +1,69 @@
+<?php
+
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019, 2020 Felix Huber
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+use Seatplus\Eveapi\Exceptions\SettingException;
+use Seatplus\Eveapi\Models\Settings\GlobalSettings;
+
+if (! function_exists('setting')) {
+
+    /**
+     * Work with settings.
+     *
+     * Providing a string argument will retrieve a setting.
+     * Providing an array argument will set a setting.
+     *
+     * @param      $name
+     * @param bool $global
+     *
+     * @return mixed
+     *
+     * @throws \Seatplus\Eveapi\Exceptions\SettingException
+     */
+    function setting($name)
+    {
+
+        // If we received an array, it means we want to set.
+        if (is_array($name)) {
+
+            // Check that we have at least 2 keys.
+            if (count($name) < 2) {
+                throw new SettingException('Must provide a name and value when setting a setting.');
+            }
+
+            return GlobalSettings::updateOrCreate(
+                ['name' => $name[0]],
+                ['value' => $name[1]]
+            )
+                ->value;
+        }
+
+
+        // If we just got a string, it means we want to get.
+        return optional(GlobalSettings::where('name', $name[0])
+            ->first())
+            ->value;
+    }
+}
