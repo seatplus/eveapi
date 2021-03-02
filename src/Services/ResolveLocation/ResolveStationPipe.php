2a049cccbe6c5378484a2688517f26394fee1d97
Herpaderp Aldent <herpaderpaldent@users.noreply.github.com>
2021-03-02 21:00:04 +0000

    Apply fixes from StyleCI

--- src/Services/ResolveLocation/ResolveStationPipe.php
+++ src/Services/ResolveLocation/ResolveStationPipe.php
@@ -1,8 +1,30 @@
 <?php
 
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019, 2020, 2021 Felix Huber
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 namespace Seatplus\Eveapi\Services\ResolveLocation;
-
 
 use Closure;
 use Seatplus\Eveapi\Actions\Location\ResolveUniverseStationByIdAction;
@@ -13,22 +35,23 @@ class ResolveStationPipe
 {
     public function __construct(
         public int $location_id
-    )
-    {
+    ) {
     }
 
     public function handle(ResolveLocationDTO $payload, Closure $next)
     {
 
         // if structure just return early
-        if(is_a($payload->location->locatable, Structure::class))
+        if (is_a($payload->location->locatable, Structure::class)) {
             return $next($payload);
+        }
 
         // if location is station and last update is greater then a week don't bother no longer
-        if(is_a($payload->location->locatable, Station::class) && $payload->location->locatable->updated_at > carbon()->subWeek())
+        if (is_a($payload->location->locatable, Station::class) && $payload->location->locatable->updated_at > carbon()->subWeek()) {
             return $next($payload);
+        }
 
-        if($this->location_id > 60_000_000 && $this->location_id < 64_000_000) {
+        if ($this->location_id > 60_000_000 && $this->location_id < 64_000_000) {
             $this->getStation();
             $payload->log_message = sprintf('successfully resolved station with id %s', $this->location_id);
         }
@@ -40,5 +63,4 @@ class ResolveStationPipe
     {
         (new ResolveUniverseStationByIdAction)->execute($this->location_id);
     }
-
 }
--- src/Services/ResolveLocation/ResolveStructurePipe.php
+++ src/Services/ResolveLocation/ResolveStructurePipe.php
@@ -1,11 +1,32 @@
 <?php
 
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019, 2020, 2021 Felix Huber
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 namespace Seatplus\Eveapi\Services\ResolveLocation;
 
-
 use Closure;
-use Seatplus\Eveapi\Actions\Location\ResolveUniverseStationByIdAction;
 use Seatplus\Eveapi\Actions\Location\ResolveUniverseStructureByIdAction;
 use Seatplus\Eveapi\Models\RefreshToken;
 use Seatplus\Eveapi\Models\Universe\Station;
@@ -16,22 +37,23 @@ class ResolveStructurePipe
     public function __construct(
         public int $location_id,
         public RefreshToken $refreshToken
-    )
-    {
+    ) {
     }
 
     public function handle(ResolveLocationDTO $payload, Closure $next)
     {
         // if station just return early
-        if(is_a($payload->location->locatable, Station::class))
+        if (is_a($payload->location->locatable, Station::class)) {
             return $next($payload);
+        }
 
         // if location is structure and last update is greater then a week don't bother no longer
-        if(is_a($payload->location->locatable, Structure::class) && $payload->location->locatable->updated_at > carbon()->subWeek())
+        if (is_a($payload->location->locatable, Structure::class) && $payload->location->locatable->updated_at > carbon()->subWeek()) {
             return $next($payload);
+        }
 
         // if location_id is a potential structure_id ( >= 100000000)
-        if($this->location_id >= 100_000_000) {
+        if ($this->location_id >= 100_000_000) {
             $this->getStructure();
             $payload->log_message = sprintf('successfully resolved structure with id %s using refresh_token of %s',
                 $this->location_id, $this->refreshToken->character->name
@@ -49,5 +71,4 @@ class ResolveStructurePipe
 
         $action->execute($this->location_id);
     }
-
 }
