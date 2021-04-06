7d9a0c5f5e5dc61ace5d798aec256c3a5ad3f5aa
Felix Huber <felix.a.huber@gmx.net>
2021-04-06 10:39:31 +0200

    Use Upsert for CorporationMemberTrackingJob.php

--- src/Jobs/Corporation/CorporationMemberTrackingJob.php
+++ src/Jobs/Corporation/CorporationMemberTrackingJob.php
@@ -101,18 +101,22 @@ class CorporationMemberTrackingJob extends NewEsiBase implements HasPathValuesIn
         }
 
         collect($response)
-            ->each(fn ($member) => CorporationMemberTracking::updateOrCreate([
+            ->map(fn($member) => [
                 'corporation_id' => $this->corporation_id,
                 'character_id'   => $member->character_id,
-            ], [
                 'start_date'   => property_exists($member, 'start_date') ? carbon($member->start_date) : null,
                 'base_id'      => $member->base_id ?? null,
                 'logon_date'   => property_exists($member, 'logon_date') ? carbon($member->logon_date) : null,
                 'logoff_date'  => property_exists($member, 'logoff_date') ? carbon($member->logoff_date) : null,
                 'location_id'  => $member->location_id ?? null,
                 'ship_type_id' => $member->ship_type_id ?? null,
+
             ])
-            )->pipe(fn ($members) => CorporationMemberTracking::where('corporation_id', $this->corporation_id)
+            ->pipe(function($members) {
+                CorporationMemberTracking::upsert($members->toArray(), ['corporation_id', 'character_id']);
+                return $members;
+            })
+            ->pipe(fn ($members) => CorporationMemberTracking::where('corporation_id', $this->corporation_id)
                 ->whereNotIn('character_id', $members->pluck('character_id')->all())->delete()
             );
     }
