70423ea958a1b5323422fd149db659b5fc11640d
Felix Huber <felix.a.huber@gmx.net>
2021-05-18 14:51:06 +0200

    Better support of nullable km properties

---
+++ src/database/migrations/2021_05_18_111734_update_killmails_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateKillmailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('killmails', function (Blueprint $table) {
+            $table->foreignId('victim_character_id')->nullable()->change();
+            $table->foreignId('victim_corporation_id')->nullable()->change();
+
+            $table->after('victim_corporation_id', function(Blueprint $table) {
+                $table->foreignId('victim_alliance_id')->nullable();
+                $table->foreignId('victim_faction_id')->nullable();
+
+                $table->unsignedBigInteger('damage_taken');
+            });
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
