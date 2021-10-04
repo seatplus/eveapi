607b71bdbc687932efd2465725cadf8a3814d59c
Felix Huber <felix.a.huber@gmx.net>
2021-10-04 12:15:17 +0200

    Remove ancestry_id and bump endpoint

---
+++ src/database/migrations/2021_10_04_101202_update_character_infos_table_remove_ancestry_id.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up()
+    {
+        Schema::table('character_infos', function (Blueprint $table) {
+            $table->dropColumn('ancestry_id');
+        });
+    }
+};
