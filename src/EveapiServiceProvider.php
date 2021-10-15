652933a298e1778e2237fb0f7a51abcfad93f68e
herpaderpaldent <herpaderpaldent@users.noreply.github.com>
2021-10-15 12:57:53 +0000

    Fix styling

--- src/EveapiServiceProvider.php
+++ src/EveapiServiceProvider.php
@@ -29,7 +29,6 @@ namespace Seatplus\Eveapi;
 use Exception;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Console\Scheduling\Schedule;
-use Illuminate\Queue\Middleware\RateLimitedWithRedis;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Schema;
@@ -106,11 +105,15 @@ class EveapiServiceProvider extends ServiceProvider
         // Add commands
         $this->addCommands();
 
-        RateLimiter::for('corporation_batch', fn ($job) => Limit::perHour(1)
+        RateLimiter::for(
+            'corporation_batch',
+            fn ($job) => Limit::perHour(1)
             ->by($job->corporation_id ?? 'corporation_batch')
         );
 
-        RateLimiter::for('character_batch', fn ($job) => Limit::perHour(1)
+        RateLimiter::for(
+            'character_batch',
+            fn ($job) => Limit::perHour(1)
             ->by($job->character_id ?? 'character_batch')
         );
     }
