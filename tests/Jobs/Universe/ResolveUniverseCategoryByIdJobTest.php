791e30863a4add5a5fa2dd82232310cda3c4f45d
Felix Huber <felix.a.huber@gmx.net>
2021-11-10 11:53:41 +0100

    Add Category and Group specific job tests

---
+++ tests/Jobs/Universe/ResolveUniverseCategoryByIdJobTest.php
@@ -0,0 +1,32 @@
+<?php
+
+
+use Illuminate\Support\Facades\Event;
+use Seatplus\Eveapi\Jobs\Universe\ResolveUniverseCategoryByIdJob;
+use Seatplus\Eveapi\Models\Universe\Category;
+use Seatplus\Eveapi\Tests\Traits\MockRetrieveEsiDataAction;
+
+uses(MockRetrieveEsiDataAction::class);
+
+it('creates category', function () {
+    $mock_data = buildCategoryMockData();
+
+    expect(Category::first())->toBeNull();
+
+    Event::fakeFor(fn() => ResolveUniverseCategoryByIdJob::dispatchSync($mock_data->category_id));
+
+    expect(Category::first())
+        ->first()->category_id->toBe($mock_data->category_id);
+
+});
+
+
+// Helpers
+function buildCategoryMockData()
+{
+    $mock_data = Category::factory()->make();
+
+    mockRetrieveEsiDataAction($mock_data->toArray());
+
+    return $mock_data;
+}
---
+++ tests/Jobs/Universe/ResolveUniverseGroupByIdJobTest.php
@@ -0,0 +1,34 @@
+<?php
+
+
+use Illuminate\Support\Facades\Event;
+use Seatplus\Eveapi\Models\Universe\Group;
+use Seatplus\Eveapi\Tests\Traits\MockRetrieveEsiDataAction;
+
+uses(MockRetrieveEsiDataAction::class);
+
+it('creates group', function () {
+    $mock_data = buildGroupMockData();
+
+    expect($mock_data)
+        ->group_id->toBeInt();
+
+    expect(Group::first())->toBeNull();
+
+    Event::fakeFor(fn() => \Seatplus\Eveapi\Jobs\Universe\ResolveUniverseGroupByIdJob::dispatchSync($mock_data->group_id));
+
+    expect(Group::first())
+        ->first()->category_id->toBe($mock_data->category_id);
+
+});
+
+
+// Helpers
+function buildGroupMockData()
+{
+    $mock_data = Group::factory()->make();
+
+    mockRetrieveEsiDataAction($mock_data->toArray());
+
+    return $mock_data;
+}
