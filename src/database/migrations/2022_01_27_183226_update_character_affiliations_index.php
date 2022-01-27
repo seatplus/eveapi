c303a925a6069d584c08966dac621e474d9a7f35
herpaderpaldent <herpaderpaldent@users.noreply.github.com>
2022-01-27 18:37:16 +0000

    Fix styling

--- src/database/migrations/2022_01_27_183226_update_character_affiliations_index.php
+++ src/database/migrations/2022_01_27_183226_update_character_affiliations_index.php
@@ -5,7 +5,6 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
-
     public function up()
     {
         Schema::table('character_affiliations', function (Blueprint $table) {
