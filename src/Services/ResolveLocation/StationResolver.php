aef3792d2037d8e2f9859f19c891cdeac5d129ee
Herpaderp Aldent <felix.a.huber@gmx.net>
2024-05-15 08:09:28 +0200

    Introduce Structure- and StationResolver
    including tests

---
+++ src/Services/ResolveLocation/StationResolver.php
@@ -0,0 +1,109 @@
+<?php
+
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019, 2020, 2021 Felix Huber
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+namespace Seatplus\Eveapi\Services\ResolveLocation;
+
+use Closure;
+use Exception;
+use Seatplus\Eveapi\Jobs\Universe\ResolveUniverseStationByIdJob;
+use Seatplus\Eveapi\Models\Universe\Location;
+use Seatplus\Eveapi\Models\Universe\Station;
+use Seatplus\Eveapi\Models\Universe\Structure;
+
+class StationResolver
+{
+    const MIN_STATION_ID = 60_000_000;
+
+    const MAX_STATION_ID = 64_000_000;
+
+    /**
+     * @throws Exception
+     */
+    public function handle(Location $location): bool
+    {
+
+        if ($this->isStructure($location) || $this->isUpdatedStation($location)) {
+            return false;
+        }
+
+        if ($this->isPotentialStation($location)) {
+            $this->handlePotentialStation($location);
+            return true;
+        }
+
+        return false;
+    }
+
+    protected function dispatchStationResolutionJob(Location $location): void
+    {
+        ResolveUniverseStationByIdJob::dispatchSync($location->location_id);
+    }
+
+    private function isStructure(Location $location): bool
+    {
+        return is_a($location->locatable, Structure::class);
+    }
+
+    private function isUpdatedStation(Location $location): bool
+    {
+        return is_a($location->locatable, Station::class) && $location->locatable->updated_at > carbon()->subWeek();
+    }
+
+    private function isPotentialStation(Location $location): bool
+    {
+        return $location->location_id > self::MIN_STATION_ID && $location->location_id < self::MAX_STATION_ID;
+    }
+
+    private function createLocation(Location $location): void
+    {
+        if (! $location->exists) {
+            $location->save();
+        }
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function handlePotentialStation(Location $location): void
+    {
+        // create location if it does not exist yet
+        $this->createLocation($location);
+
+        try {
+            // dispatch job to resolve station
+            $this->dispatchStationResolutionJob($location);
+
+            // log success
+            logger()->info("successfully resolved station with id {$location->location_id}");
+        } catch (Exception $e) {
+            // log error
+            logger()->error("failed to resolve station with id {$location->location_id}");
+
+            // report exception
+            throw $e;
+        }
+    }
+}
---
+++ tests/Jobs/Universe/ResolveLocation/StationResolverTest.php
@@ -0,0 +1,173 @@
+<?php
+
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Log;
+use Seatplus\Eveapi\Models\RefreshToken;
+use Seatplus\Eveapi\Models\Universe\Location;
+use Seatplus\Eveapi\Models\Universe\Station;
+use Seatplus\Eveapi\Models\Universe\Structure;
+use Seatplus\Eveapi\Services\ResolveLocation\StationResolver;
+use Illuminate\Support\Facades\Queue;
+
+beforeEach(function () {
+    Queue::fake();
+    Event::fake();
+});
+
+describe('isStructure or recently updated station', function () {
+
+    it('ends early if location is structure', function () {
+
+        // Arrange
+        $structure = Structure::factory()->make();
+
+        $structure->save();
+
+        $structure = Structure::first();
+
+        $location = Location::factory()->create([
+            'location_id' => $structure->structure_id,
+            'locatable_id' => $structure->structure_id,
+            'locatable_type' => Structure::class,
+        ]);
+
+        $resolveStationPipe = new StationResolver();
+
+        Log::shouldReceive()
+            ->never();
+
+        // Act
+        $result = $resolveStationPipe->handle($location);
+
+        // Assert
+        expect($result)->toBeFalse();
+    });
+
+    it('ends early if station has recently been updated', function () {
+
+        // Arrange
+        $station = Station::factory()->create([
+            'updated_at' => carbon()->subDays(6),
+        ]);
+
+        $location = Location::factory()->create([
+            'location_id' => $station->station_id,
+            'locatable_id' => $station->station_id,
+            'locatable_type' => Station::class,
+        ]);
+
+        $resolveStationPipe = new StationResolver();
+
+        Log::shouldReceive()
+            ->never();
+
+        // Act
+        $result = $resolveStationPipe->handle($location);
+
+        // Assert
+        expect($result)->toBeFalse();
+    });
+
+});
+
+describe('is potential station', function () {
+
+    beforeEach(function () {
+        Location::truncate();
+        Station::truncate();
+    });
+
+    it('returns true if station has not recently been updated', function () {
+
+        // Arrange
+        $station = Station::factory()->create([
+            'updated_at' => carbon()->subDays(8),
+        ]);
+
+        $location = Location::factory()->create([
+            'location_id' => $station->station_id,
+            'locatable_id' => $station->station_id,
+            'locatable_type' => Station::class,
+        ]);
+
+        $resolveStationPipe = new StationResolver();
+
+        // Act
+        $result = $resolveStationPipe->handle($location);
+
+        // Assert
+        expect($result)->toBeTrue();
+    });
+
+    it('creates location', function () {
+
+        // Arrange
+        $location_id = 60_005_617;
+
+        $location = Location::firstOrNew([
+            'location_id' => $location_id,
+        ]);
+
+        Log::shouldReceive('info')
+            ->once();
+
+        // Act
+        $result = (new StationResolver)->handle($location);
+
+        // Assert
+        expect(Location::count())->toBe(1)
+            ->and($result)->toBeTrue();
+
+    });
+
+    it('logs successfully resolved station', function () {
+
+        // Arrange
+        $location_id = 60_005_617;
+
+        $location = Location::firstOrNew([
+            'location_id' => $location_id,
+        ]);
+
+        Log::shouldReceive('info')->once();
+
+        // Act
+        $stationResolver = new StationResolver();
+        $result = $stationResolver->handle($location);
+
+        // Assert
+        expect($result)->toBeTrue();
+
+    });
+
+    it('throws error if resolving station fails', function () {
+
+        // Arrange
+        $location_id = 60_005_617;
+
+        expect(Location::count())->toBe(0)
+            ->and(Structure::count())->toBe(0);
+
+        $location = Location::firstOrNew([
+            'location_id' => $location_id,
+        ]);
+
+        $test_class = new class extends StationResolver {
+            protected function dispatchStationResolutionJob(Location $location): void
+            {
+                throw new \Exception('test');
+            }
+        };
+
+        Log::shouldReceive('error')->once(); // once for the error
+
+        // Act
+        $result = $test_class->handle($location);
+
+        // Assert
+        expect($result)->toBeTrue();
+    })->throws(\Exception::class);
+
+});
+
+
