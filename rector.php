2ec61adbbf346c1731dd077b7eb9b121818956dc
herpaderpaldent <felix.a.huber@gmx.net>
2024-10-06 20:57:55 +0200

    upgrade to L11 and add rector

---
+++ rector.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\SetList;
+
+return RectorConfig::configure()
+    ->withSets([
+        //SetList::DEAD_CODE,
+        //SetList::EARLY_RETURN,
+        //SetList::TYPE_DECLARATION,
+        //SetList::CODE_QUALITY,
+        //SetList::CODING_STYLE,
+        \RectorLaravel\Set\LaravelLevelSetList::UP_TO_LARAVEL_110,
+        \Rector\Set\ValueObject\LevelSetList::UP_TO_PHP_83
+    ])
+    ->withPaths([
+        __DIR__ . '/config',
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+        __DIR__ . '/database'
+    ]);
+    // uncomment to reach your current PHP version
+    // ->withPhpSets()
