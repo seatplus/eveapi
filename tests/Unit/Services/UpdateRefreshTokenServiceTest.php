aa5d8f66be19e6b79b704665b0828437b688eea4
herpaderpaldent <felix.a.huber@gmx.net>
2024-11-11 20:23:11 +0100

    UpdateRefreshTokenServiceTest.php: Refactored test to update refresh token successfully.

--- tests/Unit/Services/UpdateRefreshTokenServiceTest.php
+++ tests/Unit/Services/UpdateRefreshTokenServiceTest.php
@@ -1,29 +1,47 @@
 <?php
 
-use Seatplus\EsiClient\Services\UpdateRefreshTokenService;
+use Seatplus\EsiClient\Services\UpdateRefreshTokenService as EsiClientUpdateToken;
+use Seatplus\Eveapi\Models\RefreshToken;
+use Seatplus\EsiClient\DataTransferObjects\EsiAuthentication;
+use Seatplus\Eveapi\Services\Esi\UpdateRefreshTokenService;
 
-it('updates RefreshToken', function ($refresh_token, $access_token, $expires_in) {
-    $mock = Mockery::mock(UpdateRefreshTokenService::class);
-    $mock->shouldReceive('getRefreshTokenResponse')
-        ->andReturn([
-            'refresh_token' => $refresh_token,
-            'access_token' => $access_token,
-            'expires_in' => $expires_in,
-        ]);
-
-    // don't let event listener interfere
+beforeEach(function () {
+    $this->service = UpdateRefreshTokenService::make();
     \Illuminate\Support\Facades\Event::fake();
+});
 
-    // run the test
-    $new_token = \Seatplus\Eveapi\Services\Esi\UpdateRefreshTokenService::make()
-        ->setRefreshTokenService($mock)
-        ->update(\Seatplus\Eveapi\Models\RefreshToken::first());
-    // assert
-    expect($new_token)
-        ->refresh_token->toBeString()->toBe($refresh_token)
-        ->getRawOriginal('token')->toBeString()->toBe($access_token);
+it('updates refresh token successfully', function () {
 
-    Mockery::close();
-})->with([
-    [faker()->uuid, faker()->uuid(), faker()->randomNumber(4)],
-]);
+    $esiClientUpdateToken = mock(EsiClientUpdateToken::class, function (Mockery\MockInterface $mock) {
+
+        $mock->shouldReceive('getRefreshTokenResponse')
+            ->with(\Mockery::type(EsiAuthentication::class))
+            ->andReturn([
+                'access_token' => 'new_access_token',
+                'expires_in' => 3600,
+                'refresh_token' => 'new_refresh_token',
+            ]);
+    });
+
+    $this->service->setRefreshTokenService($esiClientUpdateToken);
+
+    $result = $this->service->update(RefreshToken::first());
+
+    expect($result)
+        ->toBeInstanceOf(RefreshToken::class)
+        ->refresh_token->toBe('new_refresh_token')
+        ->getRawOriginal('token')->toBe('new_access_token');
+});
+
+it('sets refresh token service if not set', function () {
+    $result = $this->service->getRefreshTokenService();
+    expect($result)->toBeInstanceOf(EsiClientUpdateToken::class);
+});
+
+it('uses existing refresh token service if already set', function () {
+    $esiClientUpdateToken = Mockery::mock(EsiClientUpdateToken::class);
+    $this->service->setRefreshTokenService($esiClientUpdateToken);
+
+    $result = $this->service->getRefreshTokenService();
+    expect($result)->toBe($esiClientUpdateToken);
+});
