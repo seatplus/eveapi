f4048431423a979d6957be98c2ff49ff5c8375c2
herpaderpaldent <felix.a.huber@gmx.net>
2024-11-24 23:04:54 +0100

    Add LocationRefreshTokenModelTest to test relationships.

---
+++ tests/Unit/Models/LocationRefreshTokenModelTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use Seatplus\Eveapi\Models\LocationRefreshToken;
+use Seatplus\Eveapi\Models\Universe\Location;
+
+beforeEach(function () {
+    \Illuminate\Support\Facades\Event::fake();
+});
+
+it('belongs to a location', function () {
+    $location = Location::factory()->create();
+
+    $locationRefreshToken = LocationRefreshToken::query()
+        ->updateOrCreate([
+            'location_id' => $location->location_id,
+            'character_id' => test()->test_character->character_id,
+        ], [
+            'resolved' => true,
+        ]);
+
+    expect($locationRefreshToken->location->is($location))->toBeTrue();
+});
+
+it('belongs to a refresh token', function () {
+    $location = Location::factory()->create();
+
+    $locationRefreshToken = LocationRefreshToken::query()
+        ->updateOrCreate([
+            'location_id' => $location->location_id,
+            'character_id' => test()->test_character->character_id,
+        ], [
+            'resolved' => true,
+        ]);
+
+    expect($locationRefreshToken->refresh_token->is(testCharacter()->refresh_token))->toBeTrue();
+});
