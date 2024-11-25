82d48d9a97d4746e89812c3efafe63d9001ca516
herpaderpaldent <felix.a.huber@gmx.net>
2024-11-25 08:10:34 +0100

    Add LabelModelTest to test labelable relationship creation.

---
+++ tests/Unit/Models/LabelModelTest.php
@@ -0,0 +1,11 @@
+<?php
+
+it('has labelable relationship', function () {
+    $label = \Seatplus\Eveapi\Models\Contacts\Label::factory()->create([
+        'labelable_id' => testCharacter()->character_id,
+        'labelable_type' => \Seatplus\Eveapi\Models\Character\CharacterInfo::class
+    ]);
+
+    expect($label->labelable())->toBeInstanceOf(\Illuminate\Database\Eloquent\Relations\MorphTo::class)
+        ->and($label->labelable->character_id)->toEqual(testCharacter()->character_id);
+});
