564775bae1194afd6e3d94bdc1a1e621866623b3
herpaderpaldent <felix.a.huber@gmx.net>
2024-12-19 20:27:07 +0100

    Add tests for character wallet transaction job tags array.

---
+++ tests/Unit/Jobs/CharacterWalletTransactionJobTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use Seatplus\Eveapi\Jobs\Wallet\CharacterWalletTransactionJob;
+
+it('returns correct tags array for character wallet transaction job', function () {
+    $job = new CharacterWalletTransactionJob(12345);
+
+    $tags = $job->tags();
+
+    expect($tags)->toBe([
+        'character',
+        'character_id: 12345',
+        'wallet',
+        'transaction',
+    ]);
+});
