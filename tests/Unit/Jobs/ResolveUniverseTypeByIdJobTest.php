b77fd22876ef092d7a564e80f9ee3b6c08b08c08
herpaderpaldent <felix.a.huber@gmx.net>
2024-12-19 21:02:54 +0100

    Add test for resolving universe type by ID and upserting type.

---
+++ tests/Unit/Jobs/ResolveUniverseTypeByIdJobTest.php
@@ -0,0 +1,34 @@
+<?php
+
+use Seatplus\EsiClient\DataTransferObjects\EsiResponse;
+use Seatplus\Eveapi\Jobs\Universe\ResolveUniverseTypeByIdJob;
+use Seatplus\Eveapi\Models\Universe\Type;
+
+it('executes job and upserts type', function () {
+    $data = [
+        'type_id' => 12345,
+        'group_id' => 1,
+        'name' => 'Test Type',
+        'description' => 'Test Description',
+        'published' => true,
+        'capacity' => 100,
+        'graphic_id' => 200,
+        'icon_id' => 300,
+        'market_group_id' => 400,
+        'mass' => 500,
+        'packaged_volume' => 600,
+        'portion_size' => 700,
+        'radius' => 800,
+        'volume' => 900,
+    ];
+
+    $response = new EsiResponse(json_encode($data), [], 'now', 200);
+
+    $job = mock(ResolveUniverseTypeByIdJob::class)->makePartial();
+    $job->shouldReceive('retrieve')->andReturn($response);
+
+    $job->executeJob();
+
+    expect(Type::count())->toEqual(1)
+        ->and(Type::first()->type_id)->toEqual(12345);
+});
