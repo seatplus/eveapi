0a908037ec449f18a7bfe4c67bfbf83849368a39
herpaderpaldent <felix.a.huber@gmx.net>
2024-12-23 23:24:22 +0100

    Add batch cancellation check and event fake for missing refresh token.

---
+++ tests/Unit/Jobs/HydrateTest.php
@@ -0,0 +1,69 @@
+<?php
+
+use Seatplus\Eveapi\Jobs\Hydrate\Maintenance\GetMissingBodysFromMails;
+use Seatplus\Eveapi\Jobs\Hydrate\Maintenance\GetMissingCategorys;
+use Seatplus\Eveapi\Jobs\Hydrate\Maintenance\GetMissingCharacterInfosFromCorporationMemberTracking;
+use Seatplus\Eveapi\Jobs\Hydrate\Maintenance\GetMissingConstellations;
+use Seatplus\Eveapi\Jobs\Hydrate\Maintenance\GetMissingGroups;
+use Seatplus\Eveapi\Jobs\Hydrate\Maintenance\GetMissingLocationFromAssets;
+use Seatplus\Eveapi\Jobs\Hydrate\Maintenance\GetMissingLocationFromContracts;
+use Seatplus\Eveapi\Jobs\Hydrate\Maintenance\GetMissingLocationFromCorporationMemberTracking;
+use Seatplus\Eveapi\Jobs\Hydrate\Maintenance\GetMissingLocationFromWalletTransaction;
+use Seatplus\Eveapi\Jobs\Hydrate\Maintenance\GetMissingLocations;
+use Seatplus\Eveapi\Jobs\Hydrate\Maintenance\GetMissingRegions;
+use Seatplus\Eveapi\Jobs\Hydrate\Maintenance\GetMissingTypesFromCharacterAssets;
+use Seatplus\Eveapi\Jobs\Hydrate\Maintenance\GetMissingTypesFromContractItem;
+use Seatplus\Eveapi\Jobs\Hydrate\Maintenance\GetMissingTypesFromCorporationMemberTracking;
+use Seatplus\Eveapi\Jobs\Hydrate\Maintenance\GetMissingTypesFromLocations;
+use Seatplus\Eveapi\Jobs\Hydrate\Maintenance\GetMissingTypesFromSkillQueue;
+use Seatplus\Eveapi\Jobs\Hydrate\Maintenance\GetMissingTypesFromSkills;
+use Seatplus\Eveapi\Jobs\Hydrate\Maintenance\GetMissingTypesFromWalletTransaction;
+
+it('returns early if batch is cancelled', function (string $job) {
+    $job = mock($job)->makePartial();
+
+    $job->shouldReceive('batch->cancelled')->andReturn(true);
+
+    $job->handle();
+
+    $job->shouldNotHaveReceived('batch->add');
+
+    expect(true)->toBeTrue();
+})->with([
+    GetMissingBodysFromMails::class,
+    GetMissingCategorys::class,
+    GetMissingCharacterInfosFromCorporationMemberTracking::class,
+    GetMissingConstellations::class,
+    GetMissingGroups::class,
+    GetMissingLocationFromAssets::class,
+    GetMissingLocationFromContracts::class,
+    GetMissingLocationFromCorporationMemberTracking::class,
+    GetMissingLocationFromWalletTransaction::class,
+    GetMissingLocations::class,
+    GetMissingRegions::class,
+    GetMissingTypesFromCharacterAssets::class,
+    GetMissingTypesFromContractItem::class,
+    GetMissingTypesFromCorporationMemberTracking::class,
+    GetMissingTypesFromLocations::class,
+    GetMissingTypesFromSkillQueue::class,
+    GetMissingTypesFromSkills::class,
+    GetMissingTypesFromWalletTransaction::class,
+]);
+
+it('returns null if no refresh token is found', function () {
+
+    \Illuminate\Support\Facades\Event::fakeFor(function () {
+        \Seatplus\Eveapi\Models\Mail\Mail::factory()->create();
+    });
+
+    $job = mock(GetMissingBodysFromMails::class)->makePartial();
+
+    $job->shouldReceive('batch->cancelled')->andReturn(false);
+    $job->shouldReceive('batch->add')->with([]);
+
+
+    $job->handle();
+
+
+    expect(true)->toBeTrue();
+});
