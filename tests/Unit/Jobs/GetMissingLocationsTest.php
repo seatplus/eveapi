908054e7529a9f70d77dd136d7ae83830d9b7c45
herpaderpaldent <felix.a.huber@gmx.net>
2024-12-23 22:51:49 +0100

    Add GetMissingLocationsTest.php with job cancellation test.

---
+++ tests/Unit/Jobs/GetMissingLocationsTest.php
@@ -0,0 +1,17 @@
+<?php
+
+use Seatplus\Eveapi\Jobs\Hydrate\Maintenance\GetMissingLocations;
+use Seatplus\Eveapi\Models\Universe\Location;
+
+it('adds resolve location job to batch', function () {
+
+    \Illuminate\Support\Facades\Event::fakeFor(fn () => Location::factory()->create());
+
+    $job = mock(GetMissingLocations::class)->makePartial();
+    $job->shouldReceive('batch->cancelled')->andReturn(false);
+    $job->shouldReceive('batch->add')->once();
+
+    $job->handle();
+
+    expect(true)->toBeTrue();
+});
