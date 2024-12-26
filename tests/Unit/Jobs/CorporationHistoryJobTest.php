30d15f6cde7cc93acb3b8ad422a446c0461f37ea
herpaderpaldent <felix.a.huber@gmx.net>
2024-12-26 22:57:16 +0100

    Add CorporationHistoryJobTest to test if response is cached.

---
+++ tests/Unit/Jobs/CorporationHistoryJobTest.php
@@ -0,0 +1,19 @@
+<?php
+
+use Seatplus\EsiClient\DataTransferObjects\EsiResponse;
+use Seatplus\Eveapi\Jobs\Character\CorporationHistoryJob;
+use Seatplus\Eveapi\Models\Character\CorporationHistory;
+
+it('checks if the response is cached', function () {
+    $job = mock(CorporationHistoryJob::class, function ($mock) {
+        $response = mock(EsiResponse::class, function ($mock) {
+            $mock->shouldReceive('isCachedLoad')->andReturn(true);
+        });
+
+        $mock->shouldReceive('retrieve')->andReturn($response);
+    })->makePartial();
+
+    $job->executeJob();
+
+    expect(CorporationHistory::count())->toBe(0);
+});
