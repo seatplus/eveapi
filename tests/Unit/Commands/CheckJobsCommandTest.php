325787e9f41a9c17380d2d0319623ac8af9bad61
herpaderpaldent <felix.a.huber@gmx.net>
2024-12-26 22:44:35 +0100

    Add CheckJobsCommandTest for error, warning, and success messages.

---
+++ tests/Unit/Commands/CheckJobsCommandTest.php
@@ -0,0 +1,51 @@
+<?php
+
+it('writes error, warning and success header', function (array $job_checker_result) {
+
+    $job_checker = mock(\Seatplus\Eveapi\Services\JobChecker::class, function ($mock) use ($job_checker_result) {
+        $mock->shouldReceive('checkJob')
+            ->andReturn(collect([
+                $job_checker_result,
+            ]));
+    });
+
+    $command = new \Seatplus\Eveapi\Commands\CheckJobsCommand($job_checker);
+
+    $command->handle();
+
+    expect(true)->toBeTrue();
+
+})->with([
+    'error' => fn() => [
+        'status' => 'error',
+        'message' => 'test error message'
+    ],
+    'warning' => fn() => [
+        'status' => 'warning',
+        'message' => 'test warning message'
+    ],
+    'success' => fn() => [
+        'status' => 'success',
+        'message' => 'test success message'
+    ],
+]);
+
+it('throws exception if status is unknown', function () {
+
+    $job_checker = mock(\Seatplus\Eveapi\Services\JobChecker::class, function ($mock) {
+        $mock->shouldReceive('checkJob')
+            ->andReturn(collect([
+                [
+                    'status' => 'unknown',
+                    'message' => 'test unknown message'
+                ],
+            ]));
+    });
+
+    $command = new \Seatplus\Eveapi\Commands\CheckJobsCommand($job_checker);
+
+    $command->handle();
+
+})
+    ->throws(\Exception::class, 'Unknown status');
+
