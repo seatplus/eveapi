b86828765aa23c4c41c99bf023f37cce9df2dbfb
herpaderpaldent <felix.a.huber@gmx.net>
2024-12-26 22:56:54 +0100

    Add test for checking if response is cached in CharacterInfoJobTest.

---
+++ tests/Unit/Jobs/CharacterInfoJobTest.php
@@ -0,0 +1,15 @@
+<?php
+
+it('checks if the response is cached', function () {
+    $job = mock(\Seatplus\Eveapi\Jobs\Character\CharacterInfoJob::class, function ($mock) {
+        $response = mock(\Seatplus\EsiClient\DataTransferObjects\EsiResponse::class, function ($mock) {
+            $mock->shouldReceive('isCachedLoad')->andReturn(true);
+        });
+
+        $mock->shouldReceive('retrieve')->andReturn($response);
+    })->makePartial();
+
+    $job->executeJob();
+
+    expect(\Seatplus\Eveapi\Models\Character\CharacterInfo::count())->toBe(1);
+});
