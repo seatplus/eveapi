3d86a49b21707a2a60f92099b0145145d0c0afc7
herpaderpaldent <felix.a.huber@gmx.net>
2024-12-26 23:55:09 +0100

    Add test for handling server exceptions in RetrieveFromEsiBaseTest.

---
+++ tests/Unit/RetrieveFromEsiBaseTest.php
@@ -0,0 +1,19 @@
+<?php
+
+use Seatplus\EsiClient\DataTransferObjects\EsiResponse;
+use Seatplus\EsiClient\Exceptions\RequestFailedException;
+
+it('fails when server exception is handled', function () {
+
+    $exception = new RequestFailedException(
+        new \GuzzleHttp\Exception\ServerException('failed', new \GuzzleHttp\Psr7\Request('get', 'now'), new \GuzzleHttp\Psr7\Response(404)),
+        new EsiResponse(json_encode([]), [], 'now', 200)
+    );
+
+    \Seatplus\Eveapi\Services\Facade\RetrieveEsiData::shouldReceive('execute')->andThrow($exception);
+
+   $job = new \Seatplus\Eveapi\Jobs\Assets\CharacterAssetJob(123);
+   $job->executeJob();
+
+    expect(true)->toBeTrue();
+})->throws(RequestFailedException::class);
