f391a1bb3ebcb5ab38696172f2ff8899a120a7a0
herpaderpaldent <felix.a.huber@gmx.net>
2024-12-27 23:21:32 +0100

    Refactor test for handling client exception in RetrieveFromEsiBaseTest.

--- tests/Unit/RetrieveFromEsiBaseTest.php
+++ tests/Unit/RetrieveFromEsiBaseTest.php
@@ -17,3 +17,18 @@ it('fails when server exception is handled', function () {
 
     expect(true)->toBeTrue();
 })->throws(RequestFailedException::class);
+
+it('fails when client exception is handled', function () {
+
+    $exception = new RequestFailedException(
+        new \GuzzleHttp\Exception\ClientException('failed', new \GuzzleHttp\Psr7\Request('get', 'now'), new \GuzzleHttp\Psr7\Response(404)),
+        new EsiResponse(json_encode([]), [], 'now', 200)
+    );
+
+    \Seatplus\Eveapi\Services\Facade\RetrieveEsiData::shouldReceive('execute')->andThrow($exception);
+
+    $job = new \Seatplus\Eveapi\Jobs\Assets\CharacterAssetJob(123);
+    $job->executeJob();
+
+    expect(true)->toBeTrue();
+})->throws(RequestFailedException::class);
