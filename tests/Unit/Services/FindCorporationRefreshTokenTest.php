c6451b0ff68beb05cc9e108391ba3c94bcea9325
herpaderpaldent <felix.a.huber@gmx.net>
2025-01-04 22:46:39 +0100

    Add unit test for FindCorporationRefreshToken service.

---
+++ tests/Unit/Services/FindCorporationRefreshTokenTest.php
@@ -0,0 +1,19 @@
+<?php
+
+it('returns no RefreshToken if character has no role', function () {
+
+    // Arrange
+    $corporation_id = testCharacter()->corporation_id;
+    $scope = 'esi-corporations.read_corporation_membership.v1';
+    $role = 'Director';
+
+    updateRefreshTokenScopes(testCharacter()->refresh_token, [$scope])->save();
+
+    // Act
+    $find_corporation_refresh_token = new \Seatplus\Eveapi\Services\FindCorporationRefreshToken();
+
+    $refresh_token = $find_corporation_refresh_token($corporation_id, $scope, $role);
+
+    // Assert
+    expect($refresh_token)->toBeNull();
+});
